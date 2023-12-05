5396e9a99d0352f15fa5d90975f6fea9cff7cd4e
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2023-12-05 14:09:54 -0300

    forgot to import mongoose lol

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,15 +1,16 @@
 import PostMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
 
 export const getPosts = async (req, res) =>{
     try {
        const postMessages = await PostMessage.find();
         
-       res.status(200).json({postMessages});
+       res.status(200).json(postMessages);
        
     } catch (error) {
         res.status(404).json({message: error.message}); 
     }
-};
+}
 
 export const createPost = async (req, res) =>{
     const post = req.body;
@@ -23,4 +24,15 @@ export const createPost = async (req, res) =>{
     } catch (error) {
         res.status(409).json({message: error.message});
     }
-};
+}
+
+export const updatePost = async (req, res) =>{
+    const {id: _id} = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post found with such ID')
+    
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true});
+
+    res.json(updatedPost);
+}
